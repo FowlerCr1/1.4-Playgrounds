02d075eee96f5e515cc09d87e44c54fc20852f05
Creighton Fowler Jr <fowlercr1@students.ums-wright.org>
2019-09-11 12:23:50 -0500

    First Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let ageOfCreighton = 15
+
+let ageOfKelly = 50
+
+let ageOfLydia = 15
+
+let thisYear = 2019
+
+let birthYearOfLydiaAndCreighton = 2003
+
+thisYear - birthYearOfLydiaAndCreighton
+
+128 * 2
+
+16 * 2
+
+1 * 32
+
+

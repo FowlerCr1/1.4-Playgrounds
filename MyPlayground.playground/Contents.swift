c1f0a7aa526f5db1ecdc3bf1cd320ed652d180e4
Creighton Fowler Jr <fowlercr1@students.ums-wright.org>
2019-09-11 22:20:11 -0500

    Buncha Stuffffff

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,7 +8,7 @@ let ageOfKelly = 50
 
 let ageOfLydia = 15
 
-let thisYear = 2019
+let thisYear = 2018
 
 let birthYearOfLydiaAndCreighton = 2003
 
@@ -20,10 +20,29 @@ thisYear - birthYearOfLydiaAndCreighton
 
 1 * 32
 
+//Type Testing
 let divisionTest: Double = 11
 
 let divisionTests: Double = 5
 
 divisionTest / divisionTests
 
+//Conditional Statement Testing
+let birthYearsAgo = thisYear - birthYearOfLydiaAndCreighton
 
+if thisYear == 2019 {
+    print("The year is 2019! Lydia and Creighton were born \(birthYearsAgo) years ago")
+    
+} else {
+    print("Im too dumb to do anything without the correct year...")
+}
+
+let dayOfWeek = "Tuesday"
+switch dayOfWeek {
+case "Monday" :
+    print("It's, Monday.")
+case "Tuesday" :
+    print("It's Tuesday")
+default:
+    print("It's Sunday")
+}
